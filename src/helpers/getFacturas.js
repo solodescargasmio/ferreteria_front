0775371d2cf5b809251a6fbd2a9d1ea943473694
solodescargasmio <solodescargasmio@gmail.com>
2022-04-03 15:32:04 -0300

    getfactura corregido

--- src/helpers/getFacturas.js
+++ src/helpers/getFacturas.js
@@ -3,16 +3,16 @@ import { async } from "@firebase/util";
 const getFactura = async (facturaId)=>{
     const url="http://localhost:8080/factura/"+facturaId;
     const res= await fetch(url);
-    const inventario = await res.json();
-    return inventario;
+    const factura = await res.json();
+    return factura;
      //inventario;
 }
 
 const getFacturas = async ()=>{
     const url="http://localhost:8080/factura/";
     const res= await fetch(url);
-    const inventario = await res.json();
-    return inventario;
+    const factura = await res.json();
+    return factura;
      //inventario;
 }
 export {getFacturas};
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/styles/index.sass';
-import { Provider } from 'react-redux';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
